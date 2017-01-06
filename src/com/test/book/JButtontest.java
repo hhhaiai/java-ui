e156c45e91b374ad3dff417d704039c849d576b6
yongfu.xyf <yongfu.xyf@alibaba-inc.com>
2017-01-06 15:36:50 +0800

    update test

--- src/com/test/book/JButtontest.java
+++ src/com/test/book/JButtontest.java
@@ -4,6 +4,8 @@
 package com.test.book;
 
 import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -47,12 +49,22 @@ public class JButtontest {
         b.setVerticalTextPosition(JButton.BOTTOM);
         contentPane.add(b);
         f.pack();
+        f.setSize(200, 120);
+        f.setLocation(300, 200);
         // f.show();//不推荐方法
         f.setVisible(true);
+        b.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("你点击了新的窗口");
+            }
+        });
         f.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
         });
     }
+
 }
